9ce94ab91873db822455b0f07a9ce53c5866af1e
Ahmedweb22 <ahmedwebdevelop2@gmail.com>
2025-10-04 03:33:42 +0300

    different specifier

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,3 +1,12 @@
 ﻿int x = 10;
 int y = 20;
 Console.WriteLine($"Equation: {x}+{y}={x + y:c}");
+
+
+Console.WriteLine("-----------------------------------------------------------------");
+
+//Try another examble with a different specifier with a screen shot of the out put
+double a = 0.1;
+double b = 0.2;
+
+Console.WriteLine($"Equation: {a}+{b}={a + b:p}");
